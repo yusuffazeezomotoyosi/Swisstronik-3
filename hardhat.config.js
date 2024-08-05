77f983464f4c9bc6cc12cf1f53b9fb5f8516c1a6
Yusuff Omotoyosi AZEEZ <azeezyusuff2017@gmail.com>
2024-08-05 11:29:24 +0000

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.19",
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [""],
+      accounts: [""],
     },
   },
 };
